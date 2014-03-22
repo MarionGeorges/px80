566514c07c671aea8b0287ab790a45aeb918c9c1
nadium <philinh.ngo@gmail.com>
2014-03-22 23:28:54 +0100

    Parenthèse

--- modules/blocksearch/blocksearch.php
+++ modules/blocksearch/blocksearch.php
@@ -45,7 +45,7 @@ class BlockSearch extends Module
 
 	public function install()
 	{
-		if (!parent::install() || !$this->registerHook('top') || !$this->registerHook('header') || !$this->registerHook('displayMobileTopSiteMap')) || $this->registerHook('topHead'))
+		if (!parent::install() || !$this->registerHook('top') || !$this->registerHook('header') || !$this->registerHook('displayMobileTopSiteMap') || $this->registerHook('topHead'))
 			return false;
 		return true;
 	}
